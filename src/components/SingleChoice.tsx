3134e20c1105ed616c6ac087281467666c5d0afa
Emanuel Eirich <shanks-@gmx.de>
2025-06-11 22:11:30 +0200

    fix

--- src/components/SingleChoice.tsx
+++ src/components/SingleChoice.tsx
@@ -2,6 +2,7 @@ import type { QuestionTypeProps } from '@/types/types'
 
 
 const SingleChoice = ({ options, answer, points }: QuestionTypeProps) => {
+   console.log(options, answer, points)
    return <div></div>
 }
 
